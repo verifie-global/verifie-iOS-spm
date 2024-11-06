14b3291643913a27ecec4b3f2e12f1f31ab42538
karo-dc <karo.ghazarian@gmail.com>
2024-11-07 00:29:07 +0400

    secind view

--- Sources/Verifie/Sources/View Controllers/Recommendations/RecommendationsViewController.swift
+++ Sources/Verifie/Sources/View Controllers/Recommendations/RecommendationsViewController.swift
@@ -12,6 +12,7 @@ final class RecommendationsViewController: VerifieBaseViewController, VerifieRec
     
     internal weak var videoPreviewView: VerifieVideoSessionPreviewView!
     
+    @IBOutlet weak var image1: UIImageView!
     @IBOutlet private weak var subtitleLabel: UILabel!
     @IBOutlet private weak var greatLabel: UILabel!
     @IBOutlet private weak var greatC: UIView!
@@ -36,6 +37,9 @@ final class RecommendationsViewController: VerifieBaseViewController, VerifieRec
     func update(_ textConfigs: VerifieRecommendationsTextConfigs) {
         print("JJ2 \(textConfigs)")
         title = textConfigs.title
+        
+        image1.layer.zPosition = 1
+        
         subtitleLabel.text = textConfigs.subtitle
         greatLabel.text = textConfigs.great
         noGlassesLabel.text = textConfigs.noGlasses
@@ -44,22 +48,22 @@ final class RecommendationsViewController: VerifieBaseViewController, VerifieRec
         
         greatC.layer.cornerRadius = 12
         greatC.layer.borderColor = UIColor(red: 141/255.0, green: 147/255.0, blue: 158/255.0, alpha: 1.0).cgColor
-        greatC.layer.masksToBounds = false
+        greatC.layer.masksToBounds = true
         greatC.layer.borderWidth = 1
         
         noGlassesC.layer.cornerRadius = 12
         noGlassesC.layer.borderColor = UIColor(red: 141/255.0, green: 147/255.0, blue: 158/255.0, alpha: 1.0).cgColor
-        noGlassesC.layer.masksToBounds = false
+        noGlassesC.layer.masksToBounds = true
         noGlassesC.layer.borderWidth = 1
         
         noShadowC.layer.cornerRadius = 12
         noShadowC.layer.borderColor = UIColor(red: 141/255.0, green: 147/255.0, blue: 158/255.0, alpha: 1.0).cgColor
-        noShadowC.layer.masksToBounds = false
+        noShadowC.layer.masksToBounds = true
         noShadowC.layer.borderWidth = 1
         
         noFlashC.layer.cornerRadius = 12
         noFlashC.layer.borderColor = UIColor(red: 141/255.0, green: 147/255.0, blue: 158/255.0, alpha: 1.0).cgColor
-        noFlashC.layer.masksToBounds = false
+        noFlashC.layer.masksToBounds = true
         noFlashC.layer.borderWidth = 1
 
         continueButton.layer.cornerRadius = 8
